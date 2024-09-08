24c1eae12c4f5ad1fcf687897812f45cc86e3cb3
Nick Williams <git@nickawilliams.com>
2024-09-07 21:06:42 -0700

    refactor: refactored client, moved into package index

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-export * from './pokedex-client'
+import { ResourceClient } from './lib/resource-client'
+import { Generation } from './resources/generation'
+import { Pokemon } from './resources/pokemon'
+
+class PokemonClient extends ResourceClient(Pokemon) {}
+class GenerationClient extends ResourceClient(Generation) {}
+
+export class PokedexClient {
+  public pokemon: PokemonClient
+  public generation: GenerationClient
+
+  constructor(config?: {}) {
+    this.pokemon = new PokemonClient(config)
+    this.generation = new GenerationClient(config)
+  }
+}
