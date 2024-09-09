03038f5cfe65e09e492927e1d03f56c466e5d9d1
Nick Williams <git@nickawilliams.com>
2024-09-08 17:12:52 -0700

    docs: added missing docblock

--- src/lib/named-resource.ts
+++ src/lib/named-resource.ts
@@ -1,5 +1,8 @@
 import { BaseResource } from './resource'
 
+/**
+ * Represents a named resource as defined by the PokeAPI.
+ */
 export abstract class NamedResource extends BaseResource {
   public id?: number
   public name?: string
